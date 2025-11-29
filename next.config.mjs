8c49604af160d60a0e6bc593db5d4a6f0f21d59c
redarashid <redarashid824@gmail.com>
2025-11-29 21:39:38 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     trailingSlash: true,
+//     ...(process.env.NODE_ENV === 'production' && {
+//         output: 'export',
+//         basePath: '/todo-list-next-js-15',
+//         assetPrefix: '/todo-list-next-js-15',
+//     }),
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
-    ...(process.env.NODE_ENV === 'production' && {
-        output: 'export',
-        basePath: '/todo-list-next-js-15',
-        assetPrefix: '/todo-list-next-js-15',
-    }),
+  trailingSlash: true,
+  experimental: {
+    allowedDevOrigins: ["http://192.168.1.3:3000"], // ضع هنا IP جهازك
+  },
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export",
+    basePath: "/todo-list-next-js-15",
+    assetPrefix: "/todo-list-next-js-15",
+  }),
 };
 
 export default nextConfig;
