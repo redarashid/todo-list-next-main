7c83ff57bc2c998268f331bf46f3676d9590db05
redarashid <redarashid824@gmail.com>
2025-11-30 11:34:03 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   darkMode: 'class',
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//       minHeight: {
+//         'content': 'calc(100vh - 218px)',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -13,7 +31,7 @@ module.exports = {
         foreground: "var(--foreground)",
       },
       minHeight: {
-        'content': 'calc(100vh - 218px)',
+        content: "calc(100vh - 218px)",
       },
     },
   },
